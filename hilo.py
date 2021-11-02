89b48f96e9d4e6de484afa1590264e1da202b5c7
SJD-BYUI <Spencer.damiano@gmail.com>
2021-11-02 06:23:47 -0600

    Update hilo.py
    fixed bugs and made it look pretty. Everything is ready to be submitted

--- hilo.py
+++ hilo.py
@@ -3,13 +3,12 @@ import random
 def suffle_new_game():
     cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     shuffled_cards = []
-    for card in cards:
+    for _ in cards:
         while True:
             rand_card = random.choice(cards)
             if rand_card not in shuffled_cards:
                 shuffled_cards.append(rand_card)
                 break
-    print(shuffled_cards)
     return shuffled_cards
 
 def play_game(cards):
@@ -17,34 +16,50 @@ def play_game(cards):
     turn = 0
     while points > 0:
         one = cards[turn]
-        print(f'your current points is {points}')
-        print(f'your first card is {one}')
-        guess = input('is the next card going to be higher or lower? hi/lo ')
-        # assert guess == 'hi' or guess =='lo'
+
+        if one == cards[12]:
+            play_again(points)
+            break
+
+        print()
+        print(f'your current score is {points}')
+        print(f'your card is {one}')
         turn += 1
+        guess = input('is the next card going to be higher or lower? hi/lo ')
+        print('-' * 15)
+        # assert guess == 'hi' or guess =='lo'
+        
         two = cards[turn]
-        print(f'the next card is {two}')
 
         diffrence = one - two
-        print(diffrence)
         if diffrence <= 0 and guess == 'hi' or diffrence > 0 and guess == 'lo':
             points += 100
         elif diffrence >= 0 and guess == 'hi' or diffrence < 0 and guess == 'lo':
             points -= 75
+        
+        if points == 0:
+            play_again(points)
+
+def play_again(points):
+    print(f'your score was {points}')
+    again = input('would you like to play again? ')
+    if again == 'y':
+        play_game(suffle_new_game())
+    elif again == 'n':
+        game_over(points)
+
+
+def game_over(points):
+    print('game over')
+    print(f'score - {points}')
 
 def main():
     print('welcome to hilo')
     cards = suffle_new_game()
-    print(cards)
     high_score = 0
-    run = input('would you like to play? y/n')
+    run = input('would you like to play? y/n ')
     # assert run == 'y' or run == 'n'
     if run == 'y':
         score = play_game(cards)
-        if score > high_score:
-            high_score = score
-    
-    print('Game Over')
-    print(f'Score - {high_score}')
 
 main()
